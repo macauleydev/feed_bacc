2b969942a3b4d3fdb10007e7dee181f87409c527
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-10-24 08:47:39 -0400

    Make computer codebreaker, refactor

--- game.rb
+++ game.rb
@@ -1,18 +1,39 @@
 class Game
   def initialize
     @guesses_allowed = 10
+    @guesses = []
+    @responses = []
+    @human_codebreaker = nil
+    @code = nil
+    @candidates = initial_candidates
   end
   attr_reader :guesses_allowed
+  attr_accessor :guesses, :responses, :human_codebreaker, :code, :candidates
 
-  def rating(code_string, guess_string)
+  def initial_candidates
+    list = []
+
+    ('A'..'F').each do |n1|
+      ('A'..'F').each do |n2|
+        ('A'..'F').each do |n3|
+          ('A'..'F').each do |n4|
+            list.push("#{n1}#{n2}#{n3}#{n4}")
+          end
+        end
+      end
+    end
+
+    list.shuffle!
+    starting_guesses = ['AABB', 'CCDD', 'EEFF'].shuffle
+    starting_guesses.map! {|guess| guess.split('').shuffle.join('') }
+    starting_guesses.each { |guess| list.unshift(list.delete(guess))}
+    list
+  end
+
+  def feedback(code_string, guess_string)
     code = code_string.upcase.split('').first(4)
     guess = guess_string.upcase.split('').first(4)
     tally = Hash.new(0)
-    #  {
-    #   :right_places => 0,
-    #   :wrong_places => 0,
-    #   :extraneous => 0
-    # }
 
     guess.each_with_index do |letter, index|
       right_place = index if letter == code[index]
@@ -22,8 +43,6 @@ class Game
         code[right_place] = nil
       end
     end
-    # guess.compact!
-    # code.compact!
     compact!(guess, code)
 
     guess.each_with_index do |letter, index|
@@ -47,13 +66,21 @@ class Game
 
   def play
     introduce_game
+    choose_roles
+    play_game
+  end
+
+  def replay
+    initialize
+    choose_roles
     play_game
   end
 
   def introduce_game
+    puts
     puts "           ..........................."
-    puts "           ....      CODE         ...."
-    puts "           o...    BREAKER!       o..."
+    puts "           ....      FEED-        ...."
+    puts "           o...      BACC         o..."
     puts "           *...                   oo.."
     puts "           oo..       (a          ooo."
     puts "           *o..      text-        oooo"
@@ -65,62 +92,141 @@ class Game
     puts "           oooo       code-       **o."
     puts "           *ooo     breaking,     **oo"
     puts "           **oo       game,       ***."
-    puts "           ***o      Master       ***o"
-    puts "           ****       Mind)       ****"
+    puts "           ****    Master Mind)   ****"
     puts "           ***************************"
     puts
-    puts "                The secret code"
-    puts "           is a sequence of 4 letters,"
-    puts "            each from the set, ABCDEF."
+    puts "            One player MAKES a CODE"
+    puts "           as a sequence of 4 letters,"
+    puts "           each from the set, ABCDEF."
     puts
-    puts "          (BBCB, for example. Or: FACE.)"
+    puts "         The other player BREAKS the CODE"
+    puts "           in a maximum of #{guesses_allowed} guesses."
     puts
-    puts "             You must break the code"
-    puts "              in #{guesses_allowed} guesses or fewer."
+    puts "       Each guess generates feedback such as"
     puts
-    puts "         (Guesses are not case-sensitive.)"
+    puts "           *o..        or         **oo"
     puts
-    puts "          Each guess will be rated with one"
-    puts "          '*' per correctly-placed letter, one"
-    puts "          'o' per out-of-place letter, and one"
-    puts "          '.' per superfluous letter."
+    puts "                  which tallies:"
     puts
-    # puts "      (Guesses are not case-sensitive.)"
-    # puts
-    # puts "                     Ready?"
+    puts "           *  (correctly-placed letters)"
+    puts "           o  (out-of-place letters)"
+    puts "           .  (superfluous letters)"
+    puts
     print "                       "
     gets
   end
 
-  def play_game
-    letters = %w[A B C D E F]
-    code = ''
-    4.times {code << letters.sample}
+  def choose_roles
+    puts "               Choose your role: "
+    puts "      M. Make a Code      B. Break the Code"
+    choice = ''
+    until %w[M B].include?(choice)
+      print "                       "
+      choice = gets.chomp.upcase
+    end
 
-    guesses = []
-    (1..guesses_allowed).each do |i|
-      print " Guess ##{i}? "
-      guesses.push(gets.chomp.upcase)
-      puts "   Rating: #{rating(code, guesses.last)}"
-      puts
-      break if guesses.last == code
-    end
+    self.human_codebreaker = case choice
+                             when 'B' then true
+                             when 'M' then false
+                             end
+  end
+
+  def play_game
+    make_code
+    break_code
+    puts
+
     if guesses.last == code
-      puts "Congratulations! You broke the code in #{guesses.length} guesses."
+      if self.human_codebreaker
+        print "Congratulations! You broke the "
+      else
+        print "\nWhoa! The computer broke your "
+      end
+      print "code in #{guesses.length} guesses.\n"
     else
-      puts "Sorry, you used up all #{guesses_allowed} guesses!"
-      puts "The code was #{code}."
+      puts "          So close! And yet so far."
+      puts
+      puts "              The code was #{code}."
     end
+
     puts
     print "Play again? (Y/N) "
     choice = ''
     until ['Y', 'N'].include?(choice.upcase)
       choice = gets.chomp.upcase
     end
-    play_game if choice == 'Y'
+    puts
+
+    if choice == 'Y'
+      replay
+    else
+      exit
+    end
   end
 
+  def make_code
+    if human_codebreaker
+      self.code = random_code
+    else
+      print "\nType your code here: "
+      choice = ''
+      until validate_code(choice)
+        choice = gets.chomp.upcase
+      end
+      self.code = choice
+    end
+  end
 
+  def random_code
+    letters = %w[A B C D E F]
+    code = ''
+    4.times {code << letters.sample}
+    code
+  end
+
+  def validate_code(code)
+    code.length == 4 && code.split('').all? {|n| %w[A B C D E F].include?(n.upcase) }
+  end
+
+  def break_code
+    print "\n"
+    puts "           GUESS    (case-        FEED-   "
+    puts "                  insensitive)    BACC    "
+    puts
+    (1..guesses_allowed).each do |i|
+      puts "Make your final guess!\n" if i == guesses_allowed
+      print "       "
+      print " " if i < 10
+      print "#{i}. "
+      make_guess
+      break if guesses.last == code
+      consolidate_guesses unless human_codebreaker
+    end
+  end
+
+  def make_guess
+    if human_codebreaker
+      guesses.push(gets.chomp.upcase)
+    else
+      guesses.push(candidates.first || 'WXYZ')
+      print "#{guesses.last}\n"
+    end
+
+    responses.push feedback(code, guesses.last)
+    print "                      `==>        #{responses.last}"
+    gets unless guesses.length == 10 || code == guesses.last || human_codebreaker
+    print "\n" if human_codebreaker
+  end
+
+  def consolidate_guesses
+    candidates.shift
+    candidates.each_with_index do |possible_guess, i|
+      unless feedback(possible_guess, guesses.last) == feedback(code, guesses.last)
+        self.candidates[i] = nil
+      end
+    end
+    candidates.compact!
+  end
 end
 
 new_game = Game.new
